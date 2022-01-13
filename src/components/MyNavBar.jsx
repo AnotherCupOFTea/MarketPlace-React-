5aad7b9086b1ef65647447d34361ab655a3274c6
AnotherCupOfTea <erjanengland@gmail.com>
2022-01-13 21:33:39 +0600

    d

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -1,70 +1,68 @@
-
-import React from 'react';
-import {CssBaseline, Container, Button } from '@mui/material'
+import React from "react";
+import { CssBaseline, Container, Button } from "@mui/material";
 // import { Dropdown} from 'react-bootstrap';
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import LogoNavbar from '../images/pngegg (19).png'
-import LogOutIcon from '../images/pngegg (21).png'
-import CartIcon from '../images/pngegg (10).png'
-import BurgerMenu from './BurgerMenu';
-import { AuthContext } from '../context/AuthProvider';
-
+import LogoNavbar from "../images/pngegg (19).png";
+import LogOutIcon from "../images/pngegg (21).png";
+import CartIcon from "../images/pngegg (10).png";
+import BurgerMenu from "./BurgerMenu";
+import { AuthContext } from "../context/AuthProvider";
 
 const MyNavBar = () => {
-    const {AuthWithGoogle, user, logout} = React.useContext(AuthContext)
-    console.log(user)
-    return (
-        <div >
-            <React.Fragment>
-             <div className='backgraound'></div>
-            <CssBaseline />
-                <Container fixed>
-                    <div className="navbar">
-                        <div className="navbar-left">
-                            <Link to="/">
-                            <img onClick={
-                                () =>window.scrollTo(0, 0)
-                            } width="100" src={LogoNavbar} alt="logo" />
-                            </Link>
-                            <div className="search">
-                                <input type="text" placeholder='Поиск' />
-                            </div>
-                        </div>
-                        <div className="navbar-right">
-                             {
-                                 user?(
-                                     <>
-                                         <img className='user-img' width={50} src={user.photoURL} alt="" />
-                                        <Button onClick={logout}>
-                                                <img width='50' src={LogOutIcon} alt="logout" />
-                                        </Button>
-                                    </>
-                                 ): (
-                                    <Button onClick={AuthWithGoogle} color="inherit" >
-                                    Войти
-                                  </Button>
-                                 )
-                             }
-                             <Link to="/cart">
-                                <div className="cart">
-                                    <img width={40} src={CartIcon} alt="" />
-                                </div>
-                             </Link>
-                             <div className="burger-menu">
-                              <BurgerMenu/>
-                             </div>
-                        </div>
-                    </div>
-                    
-                    
-                </Container>
-            </React.Fragment>
-
-
-
-        </div>
-    );
+  const { AuthWithGoogle, user, logout } = React.useContext(AuthContext);
+  return (
+    <div>
+      <React.Fragment>
+        <div className="backgraound"></div>
+        <CssBaseline />
+        <Container fixed>
+          <div className="navbar">
+            <div className="navbar-left">
+              <Link to="/">
+                <img
+                  onClick={() => window.scrollTo(0, 0)}
+                  width="100"
+                  src={LogoNavbar}
+                  alt="logo"
+                />
+              </Link>
+              <div className="search">
+                <input type="text" placeholder="Поиск" />
+              </div>
+            </div>
+            <div className="navbar-right">
+              {user ? (
+                <>
+                  <img
+                    className="user-img"
+                    width={50}
+                    src={user.photoURL}
+                    alt=""
+                  />
+                  <Button onClick={logout}>
+                    <img width="50" src={LogOutIcon} alt="logout" />
+                  </Button>
+                </>
+              ) : (
+                <Button onClick={AuthWithGoogle} color="inherit">
+                  Войти
+                </Button>
+              )}
+              <Link to="/cart">
+                <div className="cart">
+                  <img width={40} src={CartIcon} alt="" />
+                </div>
+              </Link>
+              <div className="burger-menu">
+                <BurgerMenu />
+              </div>
+            </div>
+          </div>
+        </Container>
+      </React.Fragment>
+    </div>
+  );
 };
 
 export default MyNavBar;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-// import ClientProvider from '../context/ClientProvider';
+import { Button } from "@mui/material";
+import React from "react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
-const ProductCard = ({product}) => {
-    console.log(product)
-    // const {} = React.useContext(ClientProvider);
-    return (
-        <div>
-            
-            <div className="card">
-                <p>{product.name}</p>
-                <img src={product.image} alt="photo" />
-                
-            </div>
+const ProductCard = ({ product }) => {
+  return (
+    <div>
+      <div className="card">
+        <p>{product.name}</p>
+        <img width="100%" src={product.image} alt="photo" />
+        <div className="buttons">
+          <Button variant="outlined">
+            <ShoppingCartIcon sx={{ fontSize: "40px", color: "black" }} />
+          </Button>
+          <Button variant="outlined">
+            <FavoriteIcon sx={{ fontSize: "40px", color: "black" }} />
+          </Button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
