73e4c309dd1e5570dc4ced34e7f3fb4aee1a67bd
AnotherCupOfTea <erjanengland@gmail.com>
2022-01-14 19:03:19 +0600

    Done Cart

---
+++ src/components/ModalPizza.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ModalPizza = () => {
+  return <div></div>;
+};
+
+export default ModalPizza;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,17 +3,36 @@ import React from "react";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import { ClientContext } from "../context/ClientProvider";
 
 const ProductCard = ({ product }) => {
+  const { addAndDeleteProductInCard, checkProductInCart } =
+    React.useContext(ClientContext);
+
   return (
     <div>
       <div className="card">
         <p>{product.name}</p>
         <img width="100%" src={product.image} alt="photo" />
         <div className="buttons">
-          <Button variant="outlined">
-            <ShoppingCartIcon sx={{ fontSize: "40px", color: "black" }} />
-          </Button>
+          {checkProductInCart(product.id) ? (
+            <Button
+              variant="outlined"
+              onClick={() => addAndDeleteProductInCard(product)}
+            >
+              <RemoveShoppingCartIcon
+                sx={{ fontSize: "40px", color: "black" }}
+              />
+            </Button>
+          ) : (
+            <Button
+              variant="outlined"
+              onClick={() => addAndDeleteProductInCard(product)}
+            >
+              <ShoppingCartIcon sx={{ fontSize: "40px", color: "black" }} />
+            </Button>
+          )}
+
           <Button variant="outlined">
             <FavoriteIcon sx={{ fontSize: "40px", color: "black" }} />
           </Button>
