0151da65745aac4f2cd6c29fa17a9ab68f728acd
AnotherCupOfTea <erjanengland@gmail.com>
2022-01-14 21:04:00 +0600

    Done cart

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { ClientContext } from "../context/ClientProvider";
+import CartTable from "../components/CartTable";
+import { Container } from "@mui/material";
 
 const CartPage = () => {
-    return (
-        <div>
-            CartPage
-        </div>
-    );
+  const { getCart, cart } = React.useContext(ClientContext);
+
+  useEffect(() => {
+    getCart();
+  }, []);
+
+  console.log(cart);
+
+  if (!cart) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (cart.products.length === 0) {
+    return <h2>Your cart is empty.</h2>;
+  }
+
+  return (
+    <div>
+      <Container maxWidth="md">
+        <CartTable cart={cart} />
+      </Container>
+      <Container maxWidth="xs"></Container>
+    </div>
+  );
 };
 
 export default CartPage;
