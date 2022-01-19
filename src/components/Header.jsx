6f5ecc08041ca5b32979951ddff17798aad60176
saparali2001 <91732916+saparali2001@users.noreply.github.com>
2022-01-19 20:43:46 +0600

    adaptiv

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Container } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HeaderImg from '../images/header-img.png'
 
 const Header = () => {
@@ -12,8 +13,8 @@ const Header = () => {
                         <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dolorum corporis, quam reiciendis velit officiis ipsam odio doloribus iste quisquam?</p>
                          <div className="header-btns">
-                             <button className='zakaz-btn'>Заказать</button>
-                             <button className='call-btn'>Позвонить</button>
+                             <a href="#pizza" className='zakaz-btn'>Заказать</a>
+                             <a  href="0755410910" className='call-btn'>Позвонить</a>
                          </div>
                     </div>
                     <div className="header-img">
